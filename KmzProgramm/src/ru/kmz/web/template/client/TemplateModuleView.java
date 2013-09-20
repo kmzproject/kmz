5a759de94b4afab36451610bc36be55777cb766b
lzibarev <lzibarev@yandex.ru>
2013-09-20 22:51:02 +0400

    Src

---
+++ KmzProgramm/src/ru/kmz/web/template/client/TemplateModuleView.java
@@ -0,0 +1,13 @@
+package ru.kmz.web.template.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class TemplateModuleView implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
