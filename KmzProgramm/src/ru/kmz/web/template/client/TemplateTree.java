c7e3ee4a80a83c2fc9abebb767b1faea3ce2ee6d
lzibarev <lzibarev@yandex.ru>
2013-09-20 23:52:53 +0400

    еще один коммит с деревом

--- KmzProgramm/src/ru/kmz/web/template/client/TemplateTree.java
+++ KmzProgramm/src/ru/kmz/web/template/client/TemplateTree.java
@@ -3,31 +3,33 @@ package ru.kmz.web.template.client;
 import ru.kmz.web.template.shared.TemplateTreeNodeBase;
 import ru.kmz.web.template.shared.TemplateTreeNodeFolder;
 
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.core.client.ValueProvider;
 import com.sencha.gxt.data.shared.ModelKeyProvider;
 import com.sencha.gxt.data.shared.TreeStore;
-import com.sencha.gxt.widget.core.client.button.ButtonBar;
-import com.sencha.gxt.widget.core.client.button.TextButton;
-import com.sencha.gxt.widget.core.client.container.FlowLayoutContainer;
-import com.sencha.gxt.widget.core.client.container.MarginData;
-import com.sencha.gxt.widget.core.client.event.SelectEvent;
-import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
 import com.sencha.gxt.widget.core.client.tree.Tree;
 
 public class TemplateTree implements IsWidget {
 
 	private TemplateTreeNodeFolder root;
 
-	public void setRoot(TemplateTreeNodeFolder root){
+	public void setRoot(TemplateTreeNodeFolder root) {
 		this.root = root;
 	}
-	
+
+	private HorizontalPanel container;
+	private TemplateTreeNodeInfo infoContainer;
+
 	@Override
 	public Widget asWidget() {
-		FlowLayoutContainer con = new FlowLayoutContainer();
-		con.addStyleName("margin-10");
+		if (container != null)
+			return container;
+		container = new HorizontalPanel();
+		container.setSpacing(10);
 
 		TreeStore<TemplateTreeNodeBase> store = new TreeStore<TemplateTreeNodeBase>(new KeyProvider());
 
@@ -38,6 +40,20 @@ public class TemplateTree implements IsWidget {
 			}
 		}
 
+		final Tree<TemplateTreeNodeBase, String> tree = getTree(store);
+
+		container.add(tree);
+
+		container.setCellWidth(tree, "260px");
+
+		infoContainer = new TemplateTreeNodeInfo();
+
+		container.add(infoContainer);
+
+		return container;
+	}
+
+	private Tree<TemplateTreeNodeBase, String> getTree(TreeStore<TemplateTreeNodeBase> store) {
 		final Tree<TemplateTreeNodeBase, String> tree = new Tree<TemplateTreeNodeBase, String>(store,
 				new ValueProvider<TemplateTreeNodeBase, String>() {
 
@@ -58,29 +74,14 @@ public class TemplateTree implements IsWidget {
 		tree.setWidth(300);
 		// tree.getStyle().setLeafIcon(ExampleImages.INSTANCE.music());
 
-		ButtonBar buttonBar = new ButtonBar();
-
-		buttonBar.add(new TextButton("Показать все", new SelectHandler() {
+		tree.getSelectionModel().addSelectionHandler(new SelectionHandler<TemplateTreeNodeBase>() {
 
 			@Override
-			public void onSelect(SelectEvent event) {
-				tree.expandAll();
+			public void onSelection(SelectionEvent<TemplateTreeNodeBase> event) {
+				infoContainer.setValue(event.getSelectedItem());
 			}
-		}));
-
-		buttonBar.add(new TextButton("Скрыть", new SelectHandler() {
-			@Override
-			public void onSelect(SelectEvent event) {
-				tree.collapseAll();
-			}
-
-		}));
-
-		buttonBar.setLayoutData(new MarginData(4));
-		con.add(buttonBar);
-
-		con.add(tree);
-		return con;
+		});
+		return tree;
 	}
 
 	private static void processFolder(TreeStore<TemplateTreeNodeBase> store, TemplateTreeNodeFolder folder) {
--- KmzProgramm/src/ru/kmz/web/template/client/TemplateTreeNodeInfo.java
+++ KmzProgramm/src/ru/kmz/web/template/client/TemplateTreeNodeInfo.java
@@ -1,5 +1,43 @@
 package ru.kmz.web.template.client;
 
-public class TemplateTreeNodeInfo {
+import ru.kmz.web.template.shared.TemplateTreeNodeBase;
 
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.widget.core.client.container.FlowLayoutContainer;
+import com.sencha.gxt.widget.core.client.form.FieldLabel;
+import com.sencha.gxt.widget.core.client.form.TextField;
+
+public class TemplateTreeNodeInfo implements IsWidget {
+
+	private FlowLayoutContainer container;
+
+	private TextField name;
+	private TextField author;
+	private TextField genre;
+
+	@Override
+	public Widget asWidget() {
+		if (container != null)
+			return container;
+		container = new FlowLayoutContainer();
+
+		// Create the fields
+		name = new TextField();
+		name.setWidth(200);
+		container.add(new FieldLabel(name, "Name"));
+
+		author = new TextField();
+		author.setWidth(200);
+		container.add(new FieldLabel(author, "Author"));
+
+		genre = new TextField();
+		genre.setWidth(200);
+		container.add(new FieldLabel(genre, "Genre"));
+		return container;
+	}
+
+	public void setValue(TemplateTreeNodeBase value) {
+		name.setValue(value.getName());
+	}
 }
